bf71dfc8ab4283134bcef1581cd216b5d62d355f
ytakahashi2020 <y.takahashi2020@gmail.com>
2024-03-27 19:55:26 +0900

    first commit

---
+++ src/components/TestWorld.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { WalletMultiButton } from "solana-wallets-vue";
+import {
+  LedgerWalletAdapter,
+  PhantomWalletAdapter,
+  // SlopeWalletAdapter,
+  TorusWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import { initWallet, useWallet } from "solana-wallets-vue";
+
+const wallets = {
+  wallets: [
+    new PhantomWalletAdapter(),
+    // new SlopeWalletAdapter(),
+    new TorusWalletAdapter(),
+    new LedgerWalletAdapter(),
+  ],
+};
+initWallet(wallets);
+
+const { connected, publicKey } = useWallet();
+</script>
+
+<template>
+  <div>
+    <p v-if="connected">public key: {{ publicKey }}</p>
+    <div v-else>
+      <wallet-multi-button></wallet-multi-button>
+    </div>
+  </div>
+</template>
